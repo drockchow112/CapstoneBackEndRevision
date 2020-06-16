2a6b6e4faae68fb55aa400734e6dcc4a71aad35b
zhongcai jiang <jiangzhongcai123@gmail.com>
2020-06-15 22:11:53 -0400

    Add more users

--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -2,8 +2,10 @@ const { User } = require("../database/models");
 
 const seedDatabase = async () => {
   await Promise.all([
-    User.create({userName: "Leon", password: "12355",email:"Leon@gmail.com",}),
-    User.create({userName: "jiang", password: "Xin2234",email:"Jiang@gmail.com"}),
+    User.create({userName: "Lamarr", password: "12355",email:"Lamarr@gmail.com",}),
+    User.create({userName: "Jiang", password: "Xin2234",email:"Jiang@gmail.com"}),
+    User.create({userName: "Derrick", password: "Xwe2234",email:"Derrick@gmail.com"}),
+    User.create({userName: "Kevin", password: "Xr22w34",email:"Kevin@gmail.com"}),
   ]);
 };
 
